e3ba20f7320cef1defe86c36806de07b45d787fc
Abhinavcv2002 <abhinavcv227@gmail.com>
2025-03-26 16:00:17 +0530

    flask

---
+++ home.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request,redirect
+import sqlite3
+App = Flask(__name__)
+
+con = sqlite3.connect("database.db")
+con.execute("CREATE TABLE IF NOT EXISTS users (name TEXT,age int)")
+
+@App.route("/",methods=['GET','POST'])
+def index():
+    if request.method=="POST":
+        name=request.form.get("name")
+        age=request.form.get("age")
+        print(name,age)
+        con = sqlite3.connect("database.db")
+        con.execute("insert into users(name,age) values(?,?)",(name,age))
+        con.commit()
+        con.close()
+        return redirect("/")
+    return render_template("index.html")
+
+App.run()
